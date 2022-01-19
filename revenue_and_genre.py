c26086c4d82ab778660b4b9adac53762c25f19ae
Andreea Pocol <andreeapocol@ymail.com>
2022-01-19 09:33:04 -0500

    Improved bar plot

--- revenue_and_genre.py
+++ revenue_and_genre.py
@@ -13,6 +13,15 @@ def addMoviesToGenreLists(revenue, movieGenres):
             genresToConsider[genre].append(revenue)
 
 
+def mean(arr):
+    avg = 0
+    t = 1
+    for x in arr:
+        avg += (x - avg) / t
+        t = t + 1
+    return avg
+
+
 def presentResults():
     genres = []
     averageRevenue = []
@@ -21,16 +30,83 @@ def presentResults():
         genres.append(genre)
         averageRevenue.append(avg)
 
-    # graph results
-    fig = plt.figure()
-    plt.bar(genres, averageRevenue, color="maroon", width=0.4)
+    mins = []
+    maxes = []
+    means = []
+    stds = []
+
     title = "The Effect of Movie Genre on Box Office Revenue"
+
+    # graph results in a box plot
+    fig = plt.figure()
+    fig.suptitle(title, wrap=True)
+    filename = "genre_vs_revenue_boxplot"
+    plt.xlabel("Genre")
+    plt.ylabel("Box Office Revenue")
+    idx = 0
+    for genre, revenue in genresToConsider.items():
+        plt.boxplot(revenue, positions=[idx], showfliers=False)
+        mins.append(min(revenue))
+        maxes.append(max(revenue))
+        means.append(np.mean(revenue))
+        stds.append(np.std(revenue))
+        idx = idx + 1
+    plt.xticks(list(range(len(genres))), genres)
+    plt.savefig(filename + ".png")
+    plt.show()
+
+    # graph results in a stacked errorbar plot
+    fig = plt.figure()
+    fig.suptitle(title, wrap=True)
+    filename = "genre_vs_revenue_errorbar_plot"
+    plt.xlabel("Genre")
+    plt.ylabel("Box Office Revenue")
+    minsToMeans = [a_i - b_i for a_i, b_i in zip(means, mins)]
+    meansToMaxes = [a_i - b_i for a_i, b_i in zip(maxes, means)]
+    plt.errorbar(np.arange(len(genres)), means, stds, fmt="ok", lw=3)
+    plt.errorbar(
+        np.arange(len(genres)),
+        means,
+        [minsToMeans, meansToMaxes],
+        fmt=".k",
+        ecolor="gray",
+        lw=5,
+    )
+    plt.yscale("log")
+    plt.xticks(list(range(len(genres))), genres)
+    plt.savefig(filename + ".png")
+    plt.show()
+
+    # graph results in a bar plot
+    fig = plt.figure()
+    plt.bar(genres, averageRevenue, color="black", width=0.4)
+    title = "Average Box Office by Genre"
     filename = "genre_vs_average_revenue"
     fig.suptitle(title, wrap=True)
     plt.xlabel("Genre")
-    plt.ylabel("Average Box Office Revenue")
+    current_values = plt.gca().get_yticks()
+    plt.gca().set_yticklabels(["${:1,.0f}".format(x) for x in current_values])
+    idx = 0
+    for genre, revenue in genresToConsider.items():
+        idx = idx + 1
+    BlockColours()
+    plt.tight_layout()
     plt.savefig(filename + ".png")
     plt.show()
+
+    # average revenue across all films
+    revenues = []
+    for genre, revenue in genresToConsider.items():
+        revenues += revenue
+    print("Average revenue for all films: {}".format(mean(revenues)))
+
+
+def BlockColours():
+    plt.axhspan(0, 1000000, facecolor="red", alpha=0.3, zorder=0)
+    plt.axhspan(1000000, 2500000, facecolor="orange", alpha=0.3, zorder=0)
+    plt.axhspan(2500000, 5000000, facecolor="yellow", alpha=0.3, zorder=0)
+    plt.axhspan(5000000, 10000000, facecolor="green", alpha=0.3, zorder=0)
+    plt.axhspan(10000000, 30000000, facecolor="blue", alpha=0.3, zorder=0)
 
 
 def processSentimentRevenueRelationshipAllGenres():
